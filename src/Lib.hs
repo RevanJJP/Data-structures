a9686a461ea080ab341352b3f3e7ec9d95ad81da
Aga <magnes.agnieszka@gmail.com>
2018-01-26 10:53:20 +0100

    some updates

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,31 +12,35 @@ import MyList
 performer :: IO ()
 performer = do
 
-  let myList1 = (myListFromList [1..1000])
+  putStrLn "Rozmiar danych: 1000"
   let binTree1 = (list2BST [1..1000])
   let avlTree1 = (buildTree [1..1000])
 
   -- umowne
-  putStrLn "Tworzenie listy: "
-  timeIt $ print (listFind 1 myList1)
   putStrLn "Tworzenie BST: "
   timeIt $ print (search 1000 binTree1)
   putStrLn "Tworzenie AVL: "
   timeIt $ print (search 512 avlTree1)
 
-  let !myList = (myListFromList $ reverse [1..1000])
-  --timeIt $ putStrLn (show myList ++ "\n" ++ "Wypisanie listy: ")
   let !binTree = (list2BST [1..1000])
-  --timeIt $ putStrLn (show binTree ++ "\n" ++ "Wypisanie drzewa BST: ")
   let !avlTree = (buildTree [1..1000])
   putStrLn "\n"
   -- umowne
-  putStrLn "Wyszukiwanie w liscie: "
-  timeIt $ print (listFind 500 myList)
   putStrLn "Wyszukiwanie w drzewie BST: "
-  timeIt $ print (search 1 binTree)
+  timeIt $ print (search 0 binTree)
   putStrLn "Wyszukiwanie w drzewie AVL: "
-  timeIt $ print (search 1 avlTree)
+  timeIt $ print (search 0 avlTree)
+
+-------------------------------------------------------------------
+  putStrLn "Rozmiar danych: 10000"
+  let !binTree = (list2BST [1..10000])
+  let !avlTree = (buildTree [1..10000])
+  putStrLn "\n"
+  -- umowne
+  putStrLn "Wyszukiwanie w drzewie BST: "
+  timeIt $ print (search 0 binTree)
+  putStrLn "Wyszukiwanie w drzewie AVL: "
+  timeIt $ print (search 0 avlTree)
 
   putStrLn "Mozna zauwazyc, ze choc czas zbudowania drzewa AVL jest znacznie wiekszy od czasu zbudowania zwyklego drzewa BST,"
-  putStrLn "to z kolei znaczaca zaleta drzewa AVL jest o wiele szybsze wyszukiwanie niz w przypadku drzewa BST."
+  putStrLn "to z kolei znaczaca zaleta drzewa AVL jest szybsze wyszukiwanie niz w przypadku drzewa BST."
--- test/Spec.hs
+++ test/Spec.hs
@@ -4,8 +4,10 @@ import AVLTree
 import BinTree.Internal
 import AVLTreeTest.UnitTests
 import BinTreeTest.UnitTests
+import MyListTest.UnitTests
 
 --main :: IO ()
 main = do
   runTestTT avlTests
   runTestTT bstTests
+  runTestTT myListTests
